bc8dcc02a9fef2ab3f3c99ede7d3d6dcfad976ac
Gonzalo Espinoza <gonzaloespinoza.alarcon@gmail.com>
2017-04-12 11:36:57 -0300

    Estructura de la solucion

---
+++ src/test/java/cl/ubb/testing/TestPila.java
@@ -0,0 +1,14 @@
+package cl.ubb.testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPila {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
